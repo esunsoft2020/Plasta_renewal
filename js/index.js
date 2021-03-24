e50545fd454da5d4d5b5bc501d6e8e77c78cc8aa
esunsoft2020 <esunsoft2020@gmail.com>
2021-03-24 23:40:43 +0900

    modify index_m.css

--- js/index.js
+++ js/index.js
@@ -37,26 +37,4 @@ function unfade(element) {
 }
 
 
-$(document).ready(function() {
-    $('.prev').click(function() {
-      $('.box').stop().animate({
-        'margin-left': '-400px'
-      }, function() {
-        $('.box div:first-child').appendTo('.box');
-        $('.box').css({
-          'margin-left': '-200px'
-        });
-      });
-    });
-  
-    $('.next').click(function() {
-      $('.box').stop().animate({
-        'margin-left': '0px'
-      }, function() {
-        $('.box div:last-child').prependTo('.box');
-        $('.box').css({
-          'margin-left': '-200px'
-        });
-      });
-    });
-  });
+
