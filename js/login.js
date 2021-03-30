1f3c7ede8f9766fcc102f9d1466c75d1c3db5845
하윤 <gkdbs514@naver.com>
2021-03-30 14:05:36 +0900

    0330-IMHAYUN

---
+++ js/login.js
@@ -0,0 +1,131 @@
+function login() {
+    const form = document.login_form;
+    const chkUsername = checkValidUsername(form);
+    const chkEmail = checkValidEmail(form);
+    const chkPw = checkValidPassword(form);
+    const chkPw2 = checkValidPassword2(form);
+
+    if (chkUsername) {
+        document.getElementById('alert_username').innerText = "";
+        form.username.style.border = '2px solid';
+        form.username.style.borderColor = '#00D000';
+    } else {
+        form.username.style.border = '2px solid';
+        form.username.style.borderColor = '#FF0000';
+        document.getElementById('alert_username').style.color = '#FF0000';
+    }
+
+    if (chkEmail) {
+        document.getElementById('alert_email').innerText = "";
+        form.email.style.border = '2px solid';
+        form.email.style.borderColor = '#00D000';
+    } else {
+        form.email.style.border = '2px solid';
+        form.email.style.borderColor = '#FF0000';
+        document.getElementById('alert_email').style.color = '#FF0000';
+    }
+
+    if (chkPw) {
+        document.getElementById('alert_password').innerText = "";
+        form.password.style.border = '2px solid';
+        form.password.style.borderColor = '#00D000';
+    } else {
+        form.password.style.border = '2px solid';
+        form.password.style.borderColor = '#FF0000';
+        document.getElementById('alert_password').style.color = '#FF0000';
+    }
+    if (chkPw2) {
+        document.getElementById('alert_password2').innerText = "";
+        form.password2.style.border = '2px solid';
+        form.password2.style.borderColor = '#00D000';
+    } else {
+        form.password2.style.border = '2px solid';
+        form.password2.style.borderColor = '#FF0000';
+        document.getElementById('alert_password2').style.color = '#FF0000';
+    }
+
+    if (chkUsername && chkEmail && chkPw && chkPw2) {
+        console.log('complete. form.submit();');
+        //form.submit();
+    }
+}
+
+function checkValidUsername(form) {
+    if (form.username.value == "") {
+        document.getElementById('alert_username').innerText = "Please enter username.";
+        //form.username.focus();
+        return false;
+    }
+
+    return true;
+}
+
+function checkValidEmail(form) {
+    if (form.email.value == "") {
+        document.getElementById('alert_email').innerText = "Please enter email.";
+        //form.email.focus();
+        return false;
+    }
+
+    const exptext = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+
+    // "ㅁ@ㅁ.ㅁ" 이메일 형식 검사.
+    if (exptext.test(form.email.value) === false) {
+        document.getElementById('alert_email').innerText = "Email is not valid.";
+        //form.email.select();
+        return false;
+    }
+
+    return true;
+}
+
+function checkValidPassword(form) {
+    if (form.password.value == "") {
+        document.getElementById('alert_password').innerText = "Please enter password.";
+        //form.password.focus();
+        return false;
+    }
+
+    const pw = form.password.value;
+    // String.prototype.search() :: 검색된 문자열 중에 첫 번째로 매치되는 것의 인덱스를 반환한다. 찾지 못하면 -1 을 반환한다.
+    // number
+    const num = pw.search(/[0-9]/g);
+    // alphabet
+    const eng = pw.search(/[a-z]/ig);
+    // special characters
+    const spe = pw.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+
+    if (pw.length < 6) {
+        // 최소 6문자.
+        document.getElementById('alert_password').innerText = "Password must be at least 6 characters.";
+        return false;
+    } else if (pw.search(/\s/) != -1) {
+        // 공백 제거.
+        document.getElementById('alert_password').innerText = "Please enter your password without spaces.";
+        return false;
+    } else if (num < 0 && eng < 0 && spe < 0) {
+        // 한글과 같은 문자열 입력 방지.
+        document.getElementById('alert_password').innerText = "Password is not valid.";
+        return false;
+    }
+
+    return true;
+}
+
+function checkValidPassword2(form) {
+    if (form.password2.value == "") {
+        document.getElementById('alert_password2').innerText = "Password2 is required.";
+        //form.password.focus();
+        return false;
+    }
+
+    if (form.password.value !== form.password2.value) {
+        document.getElementById('alert_password2').innerText = "Password and password2 is not match.";
+        form.password.style.border = '2px solid';
+        form.password.style.borderColor = '#FF0000';
+        document.getElementById('alert_password').style.color = '#FF0000';
+        return false;
+    }
+
+    return true;
+}
